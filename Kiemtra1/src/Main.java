9487ff1e8c1aff7ae6b47af7c72016fc7bad8a6e
Mcuong248 <ngmanhcuong2001@gmail.com>
2021-12-03 06:32:31 +0700

    Kiemtra1

---
+++ Kiemtra1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Câu 2");
+        String str = "You only live once, but if you do it right, once is enough";
+        System.out.println("Số từ có trong chuỗi: " + Cau2.countWord(str));
+        Cau2.countCharater(str);
+
+        System.out.println("Câu 4");
+        Cau4.printPrimeNumberLessThan10(10);
+        Cau4.printFirst10PrimeNumber(10);
+    }
+}
